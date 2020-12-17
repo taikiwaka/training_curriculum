8e87cdd44c498b865df9ed636ed3433fcacf8097
taikiwaka <60640210+taikiwaka@users.noreply.github.com>
2020-12-17 18:57:37 +0900

    wdayメソッドなどの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,9 +18,8 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def get_Week
+  def get_week
   wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-  end
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -36,14 +35,14 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
+      wday_num = Date.today.wday + x # その日の日にちを表示するDate.today.wdayにxのブロック変数を足す- wdayメソッドを用いて取得した数値
       if ( wday_num >= 7 ) #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday =>　wdays[wday_num].wday } # wdaysから値を取り出す記述
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] } # wdaysから値を取り出す記述
       @week_days.push(days)
     end
+  end
 
   end
-end
