c5080085e1fb3e035451a2e37a34cdc1028be2c2
taikiwaka <60640210+taikiwaka@users.noreply.github.com>
2020-12-17 19:57:06 +0900

    ハッシュロケットからシンプル型へ

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -40,7 +40,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] } # wdaysから値を取り出す記述
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] } # wdaysから値を取り出す記述
       @week_days.push(days)
     end
   end
